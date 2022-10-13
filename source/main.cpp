a991dcf912f189006d064d88f76340b435429e88
Vavik16 <114207567+Vavik16@users.noreply.github.com>
2022-10-14 01:50:31 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <cstring>
 #include <bitset>
+#include <algorithm>
 
 void func(std::vector<std::string> arg);
 
@@ -28,42 +29,60 @@ void func(std::vector<std::string> arg) {
     const std::string mode = arg[0];
     const std::string file_namearg = arg[1];
     if (mode == "encryption") {
-        std::ifstream ifin;
-        ifin.open(file_namearg);
-        if (ifin) {
-            std::string enc1;
-            std::getline(ifin, enc1);
-            ifin.close();
-            srand(time(0));
-            int a = rand()%9000 + 1000;
-            std::string key1 = std::to_string(a);
-            char key = key1[2];
-            std::bitset<8> m(key);
-            std::ofstream ofin;
-            ofin.open(file_namearg, std::ofstream::out | std::ofstream::trunc);
-            for (size_t i = 0; i < enc1.size(); i++) {
-                std::bitset<8> x(enc1[i]);
-                x = x ^ m;
-                x = (x >> 2) | (x << 6);
-                unsigned long ii = x.to_ulong();
-                unsigned char c = static_cast<unsigned char>(ii);
-                enc1[i] = c;
-                ofin << enc1[i];
-            }
-            char pass = 'X';
-            for (size_t i = 0; i < key1.size(); i++) {
-                std::bitset<8> x(key1[i]);
-                std::bitset<8> y(pass);
-                x = x ^ y;
-                x = (x >> 2) | (x << 6);
-                unsigned long ii = x.to_ulong();
-                unsigned char c = static_cast<unsigned char>(ii);
-                key1[i]=c;
-            }
-            ofin << key1;
-            std::cout << "Encryption finished!" << std::endl;
-            ofin.close();
-        } else std::cerr << "Error: Write correct filename" << std::endl;
+        std::string enc1, enc2;
+        int pass1;
+        std::cout << "Please, enter string which you want to encrypt" << std::endl;
+        std::cin >> enc1;
+        getline(std::cin, enc2);
+        enc1 += enc2;
+        if (enc1.size() > 248) std::cerr << "Error: Write correct string (<=248)" << std::endl;
+        else {
+            std::cout << "Please, enter password: " << std::endl;
+            if (std::cin >> pass1) {
+                srand(time(nullptr));
+                int a = rand() % 9000 + 1000;
+                std::string key1 = std::to_string(a);
+                std::string pass = std::to_string(pass1);
+                char key = key1[2];
+                std::bitset<8> m(key);
+                std::ofstream ofin;
+                ofin.open(file_namearg, std::ofstream::out | std::ofstream::trunc);
+                if (ofin) {
+                    for (char &i: enc1) {
+                        std::bitset<8> x(i);
+                        x = x ^ m;
+                        x = (x >> 2) | (x << 6);
+                        unsigned long ii = x.to_ulong();
+                        auto c = static_cast<unsigned char>(ii);
+                        i = c;
+                        ofin << i;
+                    }
+                    char passw = 'X';
+                    for (char &pas: pass) {
+                        std::bitset<8> x(pas);
+                        std::bitset<8> y(passw);
+                        x = x ^ y;
+                        x = (x >> 2) | (x << 6);
+                        unsigned long ii = x.to_ulong();
+                        auto c = static_cast<unsigned char>(ii);
+                        pas = c;
+                    }
+                    ofin << "PS01" << pass;
+                    for (char &i: key1) {
+                        std::bitset<8> x(i);
+                        std::bitset<8> y(passw);
+                        x = x ^ y;
+                        x = (x >> 2) | (x << 6);
+                        unsigned long ii = x.to_ulong();
+                        auto c = static_cast<unsigned char>(ii);
+                        i = c;
+                    }
+                    ofin << "NIx0" << key1;
+                    std::cout << "Encryption finished!" << std::endl;
+                    ofin.close();
+                } else std::cerr << "Error: Write correct filename" << std::endl;
+            } else std::cerr << "Error: Write correct password" << std::endl;
+        }
     } else if (mode == "decryption") {
         std::ifstream ifin;
         ifin.open(file_namearg);
@@ -73,29 +92,42 @@ void func(std::vector<std::string> arg) {
             char pass = 'X';
             std::bitset<8> y(pass);
             std::string key;
-            int m = dec.size();
-           for(int i = m-1; i >= m-4; --i) {
+            std::string password;
+            std::string password_check;
+            size_t pos1 = dec.find("PS01");
+            size_t pos2 = dec.find("NIx0");
+            size_t m = dec.size();
+            for (size_t i = pos1 + 4; i < pos2; i++) {
                 std::bitset<8> x(dec[i]);
                 x = (x << 2) | (x >> 6);
                 x = x ^ y;
                 unsigned long ii = x.to_ulong();
-                unsigned char c = static_cast<unsigned char>(ii);
-                key += c;
-                dec.erase(i);
+                auto c = static_cast<unsigned char>(ii);
+                password += c;
             }
-            std::bitset<8> y1(key[1]);
-            std::ofstream ofin;
-            ofin.open(file_namearg, std::ofstream::out | std::ofstream::trunc);
-            for (size_t i = 0; i < dec.size(); i++) {
+            for (size_t i = pos2 + 4; i < m; i++) {
                 std::bitset<8> x(dec[i]);
                 x = (x << 2) | (x >> 6);
-                x = x ^ y1;
+                x = x ^ y;
                 unsigned long ii = x.to_ulong();
-                unsigned char c = static_cast<unsigned char>(ii);
-                dec[i] = c;
-                ofin << dec[i];
+                auto c = static_cast<unsigned char>(ii);
+                key += c;
             }
-            std::cout << "Decryption finished! Your phrase is: " << dec << " Also you can see it in your file!" << std::endl;
+            dec.erase(dec.begin() + pos1, dec.end());
+            std::cout << "Enter password: " << std::endl;
+            std::cin >> password_check;
+            if (password_check == password) {
+                std::bitset<8> y1(key[2]);
+                for (char & i : dec) {
+                    std::bitset<8> x(i);
+                    x = (x << 2) | (x >> 6);
+                    x = x ^ y1;
+                    unsigned long ii = x.to_ulong();
+                    auto c = static_cast<unsigned char>(ii);
+                    i = c;
+                }
+                std::cout << "Decryption finished! Your phrase is: " << dec << std::endl;
+            } else std::cerr << "Error: Write correct password" << std::endl;
         } else std::cerr << "Error: Write correct filename" << std::endl;
     }
 }
